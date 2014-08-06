2da9b093774c11d21c5af288365ea41502aaff81
Julien-JNS <julien.jaunais@gmail.com>
2014-08-07 01:29:36 +0200

    init test

---
+++ src/fr/jjj/conductor/model/DeviceVideoIn.java
@@ -0,0 +1,12 @@
+package fr.jjj.conductor.model;
+
+/**
+ * Created by Jaunais on 05/08/2014.
+ */
+public class DeviceVideoIn extends Device{
+
+
+    public DeviceVideoIn(String label) {
+        super(label);
+    }
+}
