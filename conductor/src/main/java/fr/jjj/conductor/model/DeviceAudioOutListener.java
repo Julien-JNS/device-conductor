fc589c45cf79a822aeecc30784482784884581be
Julien-JNS <julien.jaunais@gmail.com>
2014-10-10 18:37:12 +0200

    Module approach

---
+++ conductor/src/main/java/fr/jjj/conductor/model/DeviceAudioOutListener.java
@@ -0,0 +1,16 @@
+package fr.jjj.conductor.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jjaunais
+ * Date: 07/10/14
+ * Time: 15:40
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DeviceAudioOutListener {
+
+    void nowPlaying(MediaItem item);
+
+
+
+}
---
+++ conductor/src/main/java/fr/jjj/conductor/model/TestHandler.java
@@ -0,0 +1,15 @@
+package fr.jjj.conductor.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jjaunais
+ * Date: 07/10/14
+ * Time: 15:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestHandler extends PlayerHandler {
+
+    public TestHandler() {
+        super("sleep", "10",Resource.ItemArgFormat.NONE);
+    }
+}
