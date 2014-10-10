7e819057e7750c96f77e8c4a778f6e78fae3e259
Julien-JNS <julien.jaunais@gmail.com>
2014-10-10 18:37:53 +0200

    Module approach

---
+++ conductor-manager/src/test/java/fr/jjj/conductormanager/standalone/TestStandalone.java
@@ -0,0 +1,61 @@
+package fr.jjj.conductormanager.standalone;
+
+import fr.jjj.conductor.Conductor;
+import fr.jjj.conductor.ConductorFactory;
+import fr.jjj.conductormanager.TestUtils;
+import fr.jjj.conductormanager.ui.StatusPresenter;
+import fr.jjj.conductormanager.ui.StatusView;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jjaunais
+ * Date: 02/10/14
+ * Time: 14:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStandalone {
+
+    @Mock
+    StatusView statusView;
+
+    private  StatusPresenter statusPresenter;
+
+    private  StatusPresenter statusPresenter;
+
+    @Before
+    public void initialize() {
+        TestUtils.initializeConfig();
+        TestUtils.initializeFileSystem();
+        TestUtils.initializeManagerConfig();
+        Conductor conductor=ConductorFactory.getInstance().getConductor();
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testStatus()
+    {
+        
+        statusPresenter=new StatusPresenter(statusView);
+        Mockito.verify(statusView).setDetailedStatus(Matchers.anyString());
+        Mockito.verify(statusView).setGlobalStatus(Matchers.anyString());
+
+    }
+
+    @Test
+    public void testDeviceList()
+    {
+
+        statusPresenter=new StatusPresenter(statusView);
+        Mockito.verify(statusView).setDetailedStatus(Matchers.anyString());
+        Mockito.verify(statusView).setGlobalStatus(Matchers.anyString());
+
+    }
+}
